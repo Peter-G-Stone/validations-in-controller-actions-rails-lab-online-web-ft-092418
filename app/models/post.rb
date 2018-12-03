4eda76ff0bb793df1548e87c53d31c211e53f14f
Peter-G-Stone <peter.stone.online@gmail.com>
2018-12-03 12:59:42 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
+# class MyValidator < ActiveModel::Validator
+#     def validate(record)
+#         phr = ["Won't Believe", "Secret", "Top", "Guess"]
+#         if record.title
+#             unless phr.any? { |p| record.title.include?(p)} 
+#                 record.errors[:name] << 'Need a clickbait title please'
+#             end
+#         end
+#     end
+# end
+
 class Post < ActiveRecord::Base
+    # include ActiveModel::Validations
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    # validates_with MyValidator
 end
